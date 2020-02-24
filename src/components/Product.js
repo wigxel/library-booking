920dbf5cc4b26bc43c98294e5c0f19e34ddd1272
Joseph Julius <Joseph.owonwo@gmail.com>
2020-02-24 13:18:47 +0100

    fix: :bug: invoice selection

--- src/components/Product.js
+++ src/components/Product.js
@@ -109,7 +109,7 @@ export const ProductDetails = () => {
             options={Select.options(["Worksheet", "Invoice"])}
             onChange={(val) => setOption(val)}>
           </Select>
-          <Button disabled={option !== 'Invoice'}>
+          <Button disabled={option.text !== 'Invoice'}>
             Print
           </Button>
           <button className="border border-gray-200 rounded-lg px-4 py-2 text-sm mx-2">
