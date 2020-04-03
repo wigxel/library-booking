22e0f531e590eb18b5e8abed1a1cdacbec9af95a
DouglasDivine <douglaschinenye@yahoo.com>
2020-04-03 15:38:29 +0200

    persist data book state to localstorage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import React from 'react';
 import AddBook from './components/AddBookFunction'
 import './App.css';
 import Book from './components/Book'
+ 
+
+//JSON.parse() // cast string -> object
+// Retreive the existing data from localStorage
+// Set that data as the default state for the App component
+localStorage.getItem("book") // string | null
+//localStorage.getItem("users") // -> null
+// useState | useEffect 
+
+// persist data book state to localStorage
 
 function App() {
+  const data =  JSON.parse(localStorage.getItem("books"))
   // create a state
-  const [state,setState]= React.useState([])
+  const [state, setState] = React.useState(data ? data : [])
+
   const handleSubmitBook = (bookData) => {
-    setState([ ...state, bookData ]);
-    //console.log('Data was submited', bookData)
+    const newState = [ ...state, bookData ]
+    setState(newState);
+    
+    localStorage.setItem("books", JSON.stringify(newState));
   }
 
-  return (
+  return ( 
     <div className='container'>
       <section>
         <AddBook onSubmitBook={handleSubmitBook}/>
